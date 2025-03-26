867ba41a2837ee6cb90a5a744391e511b61727d4
MartinRenz <martinrenz@outlook.com>
2025-03-26 17:00:50 -0300

    - Adiciona comentários e remove espaços em branco na classe de fatura

--- TheatricalPlayersRefactoringKata.Core/Entities/Invoice.cs
+++ TheatricalPlayersRefactoringKata.Core/Entities/Invoice.cs
@@ -2,12 +2,22 @@ using System.Collections.Generic;
 
 namespace TheatricalPlayersRefactoringKata.Core.Entities;
 
+/// <summary>
+/// Representa uma fatura de uma peça teatral.
+/// </summary>
 public class Invoice
 {
     private string _customer;
     private List<Performance> _performances;
 
+    /// <summary>
+    /// Nome do cliente associado a fatura.
+    /// </summary>
     public string Customer { get => _customer; set => _customer = value; }
+
+    /// <summary>
+    /// Lista de performances associadas ao cliente.
+    /// </summary>
     public List<Performance> Performances { get => _performances; set => _performances = value; }
 
     public Invoice(string customer, List<Performance> performance)
@@ -15,5 +25,4 @@ public class Invoice
         this._customer = customer;
         this._performances = performance;
     }
-
 }
