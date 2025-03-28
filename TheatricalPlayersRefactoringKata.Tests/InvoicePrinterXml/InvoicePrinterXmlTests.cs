6a8ce5714b7fcea4505f31f3e894a4752d38ddd4
MartinRenz <martinrenz@outlook.com>
2025-03-27 21:48:02 -0300

    - Adiciona teste para validar a estrutura do XML

--- TheatricalPlayersRefactoringKata.Tests/InvoicePrinterXml/InvoicePrinterXmlTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/InvoicePrinterXml/InvoicePrinterXmlTests.cs
@@ -2,6 +2,7 @@ using ApprovalTests;
 using ApprovalTests.Reporters;
 using System;
 using System.Collections.Generic;
+using System.Xml;
 using TheatricalPlayersRefactoringKata.Core.Entities;
 using TheatricalPlayersRefactoringKata.Core.Enums;
 using TheatricalPlayersRefactoringKata.Application.Services.Invoices;
@@ -39,6 +40,48 @@ public class InvoicePrinterXmlTests
 
         Approvals.Verify(result);
     }
+
+    [Fact]
+    public void ShouldGenerateValidXmlStructure()
+    {
+        var invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance(new Play("Hamlet", 4024, PlayType.Tragedy), 55),
+                new Performance(new Play("As You Like It", 2670, PlayType.Comedy), 35),
+                new Performance(new Play("Othello", 3560, PlayType.Tragedy), 40)
+            }
+        );
+
+        var result = _invoicePrinterXml.Print(invoice);
+
+        // Load the XML into XmlDocument
+        var xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(result);
+
+        Assert.NotNull(xmlDoc.DocumentElement);
+
+        // Assert the structure of the XML
+        Assert.Equal("Statement", xmlDoc.DocumentElement.Name);
+
+        // Validate the presence of the Customer element
+        var customerElement = xmlDoc.SelectSingleNode("//Customer");
+        Assert.NotNull(customerElement);
+        Assert.Equal("BigCo", customerElement.InnerText);
+
+        // Validate presence of Item elements inside the Items element
+        var itemElements = xmlDoc.SelectNodes("//Item");
+        Assert.NotNull(itemElements);
+        Assert.True(itemElements.Count > 0);
+
+        // Validate specific structure of the first Item
+        var firstItem = itemElements[0];
+        Assert.NotNull(firstItem.SelectSingleNode("AmountOwed"));
+        Assert.NotNull(firstItem.SelectSingleNode("EarnedCredits"));
+        Assert.NotNull(firstItem.SelectSingleNode("Seats"));
+    }
+
 
     [Fact]
     public void ShouldThrowArgumentNullException_WhenInvoiceIsNull()
